a0e30970c02209e21787848c81f444c12d575b62
forest11 <350428523@qq.com>
2017-03-15 23:42:09 +0800

    2.0

--- mysite/monitor/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def product_detail(request):
-    return render(request, 'monitor/product_detail.html')
-
-
-def zabbix(request):
-    return render(request, 'common/index.html')
